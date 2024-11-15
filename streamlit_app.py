293c591a4f40d1ca003f10c5c0005a30f7b9381e
miju2422 <20242422@sdhs.gwe.hs.kr>
2024-11-15 15:19:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,10 @@ import streamlit as st
 import pandas as pd
 
 # 1. 제목
-st.title("Streamlit 기본 예제 페이지")
+st.title("전미주의 인터넷 서비스")
 
 # 2. 부제목
-st.subheader("이 페이지는 다양한 Streamlit 기능을 보여줍니다.")
+st.subheader("정보수행 하기 싫어요...너무 어려워요")
 
 # 3. 판다스 데이터프레임 기반 표 출력
 df = pd.DataFrame({
